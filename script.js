855dbd3e0be8ebbaad512c0c990b0e62666df0a8
Shivansh Kumar <56130670+ShivanshKumar@users.noreply.github.com>
2020-07-22 15:35:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let schedule = [
+[{sub: 0, timehr: 8 ,timemin: 0},{sub: 1, timehr: 9 ,timemin: 0},{sub: 2, timehr: 10 ,timemin:15},{sub: 3, timehr: 11 ,timemin: 15}],
+[{sub: 4, timehr: 8 ,timemin: 0},{sub: 6, timehr: 10 ,timemin: 15},{sub: 0, timehr: 11 ,timemin: 15}],
+[{sub: 1, timehr: 8 ,timemin: 0},{sub: 2, timehr: 9 ,timemin: 0},{sub: 4, timehr: 11 ,timemin: 15}],
+[{sub: 6, timehr: 9 ,timemin: 0},{sub: 0, timehr: 10 ,timemin:15},{sub: 1, timehr: 11 ,timemin: 15}],
+[{sub: 2, timehr: 8 ,timemin: 0},{sub: 4, timehr: 10 ,timemin:15}]
+]
+let d = new Date();
+let day = d.getDay();
+let UTCdate = d.getUTCDate();
+let a =document.querySelectorAll(`a`)
+let today = schedule[day-1]
+
+console.log(a,day,UTCdate,today,schedule)
+
+today.forEach((v)=>{
+console.log(v);
+let fun = ()=>{
+a[v.sub].click();}
+let eta_ms = ()=> new Date(2020, 6, UTCdate, v.timehr, v.timemin).getTime() - Date.now();
+if(eta_ms>0)setTimeout(fun,eta_ms());
+})
+
---
+++ tampermonkey.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://meet.google.com/rwy-cafc-zus
+// @match        http://meet.google.com/yaf-untz-smm
+// @match        https://meet.google.com/jnr-zmjz-uko
+// @match        https://meet.google.com/duz-vzzh-rso
+// @match        https://meet.google.com/edt-kaag-azn
+// @match        https://meet.google.com/gov-hpkt-xgq
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    var x = document.getElementsByClassName("RveJvd snByac");
+    var fun = ()=>{
+        x[4].click();
+        x[0].click();
+    }
+    setTimeout(fun,5000);
+
+    var fun2 = ()=>{
+     var z = document.getElementsByClassName("U26fgb JRY2Pb mUbCce kpROve GaONte Qwoy0d ZPasfd vzpHY");
+     z[0].click();
+    }
+    setTimeout(fun2,3300000);
+})();
