e854df219c3bfcc26a62aef9727fd729838100cb
Shivansh Kumar <56130670+ShivanshKumar@users.noreply.github.com>
2020-07-27 00:23:51 +0530

    Added bunch of console logs

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let schedule = [
 [{sub: 6, timehr: 9 ,timemin: 0},{sub: 0, timehr: 10 ,timemin:15},{sub: 1, timehr: 11 ,timemin: 15}],
 [{sub: 2, timehr: 8 ,timemin: 0},{sub: 4, timehr: 10 ,timemin:15}]
 ]
+
 let d = new Date();
 let day = d.getDay();
 let date = d.getDate();
@@ -16,8 +17,10 @@ console.log(a,day,date,today,schedule)
 today.forEach((v)=>{
 console.log(v);
 let fun = ()=>{
+console.log(v.sub);	
 a[v.sub].click();}
 let eta_ms = ()=> new Date(2020, 6, date, v.timehr, v.timemin).getTime() - Date.now();
-if(eta_ms()>0)setTimeout(fun,eta_ms());
+console.log(eta_ms());
+if(eta_ms()>0)setTimeout(fun,eta_ms());	
 })
 
